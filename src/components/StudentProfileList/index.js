f7ffdaf81a76b1775786796a45b366cc4355ec69
surajhell88 <surajhell88@gmail.com>
2020-06-09 16:20:07 +0530

    Adding search input for querying by first and last name of the student, Part 3 completed

--- src/components/StudentProfileList/index.js
+++ src/components/StudentProfileList/index.js
@@ -2,12 +2,32 @@ import React, { useEffect, useState } from 'react'
 import './index.scss'
 import { getStudentProfiles } from '../../utils/api'
 
+function matchStrings(string1, string2) {
+  return string1.toLowerCase().indexOf(string2.toLowerCase()) !== -1;
+}
+
 function StudentProfileList() {
+  const [searchKey, setSearchKey] = useState('')
   const [studentList, setStudentList] = useState([])
-  useEffect(() => {
+  const [filteredList, setFilteredList] = useState([])
+  function handleChange(e) {
+    setSearchKey(e.target.value)
+  }
+  useEffect(() => { // filter by search input value
+    if (searchKey) {
+      const filteredList = studentList.filter(student => {
+        const { firstName, lastName } = student
+        return matchStrings(firstName, searchKey) || matchStrings(lastName, searchKey)
+      })
+      setFilteredList(filteredList)
+    } else {
+      setFilteredList(studentList)
+    }
+  }, [searchKey, studentList])
+  useEffect(() => { // fetch student profiles once
     getStudentProfiles().then(response => {
       const { students } = response.data
-      setStudentList(students.map(student => {
+      const listOfProfiles = students.map(student => {
         const average = student.grades.reduce((accumulator, grade) => {
           return accumulator + Number(grade)
         }, 0) / student.grades.length
@@ -15,28 +35,39 @@ function StudentProfileList() {
           ...student,
           average,
         }
-      }))
+      })
+      setStudentList(listOfProfiles)
+      setFilteredList(listOfProfiles)
     })
   }, [])
   return (
-    <ul className="student-profile-list">
-      {studentList.map(profile => {
-        return <li key={profile.id}>
-          <div className="student-profile-list-container">
-            <div className="student-profile-list-image">
-              <img src={profile.pic} alt={`${profile.firstName} ${profile.lastName}`} />
+    <div className="student-profile">
+      <input
+        className="student-profile-search"
+        type="text"
+        value={searchKey}
+        onChange={handleChange}
+        placeholder="Search..."
+      />
+      <ul className="student-profile-list">
+        {filteredList.map(profile => {
+          return <li key={profile.id}>
+            <div className="student-profile-list-container">
+              <div className="student-profile-list-image">
+                <img src={profile.pic} alt={`${profile.firstName} ${profile.lastName}`} />
+              </div>
+              <div className="student-profile-list-info">
+                <h4>{`${profile.firstName} ${profile.lastName}`}</h4>
+                <p>Email: {profile.email}</p>
+                <p>Company: {profile.company}</p>
+                <p>Skill: {profile.skill}</p>
+                <p>Average: {profile.average}%</p>
+              </div>
             </div>
-            <div className="student-profile-list-info">
-              <h4>{`${profile.firstName} ${profile.lastName}`}</h4>
-              <p>Email: {profile.email}</p>
-              <p>Company: {profile.company}</p>
-              <p>Skill: {profile.skill}</p>
-              <p>Average: {profile.average}%</p>
-            </div>
-          </div>
-        </li>
-      })}
-    </ul>
+          </li>
+        })}
+      </ul>
+    </div>
   );
 }
 
