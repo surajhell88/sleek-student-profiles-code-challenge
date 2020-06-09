98c5ac09f73cfb4e6bc9389cd51ce62526f47150
surajhell88 <surajhell88@gmail.com>
2020-06-09 21:16:47 +0530

    Naming variable matchClause instead of searchClause

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -2,13 +2,13 @@ function matchStrings(firstString, secondString) {
     return firstString.toLowerCase().indexOf(secondString.toLowerCase()) !== -1
 }
 
-function filter(listOfData, arrOfKeys, searchKey, searchClause = 'OR') {
+function filter(listOfData, arrOfKeys, searchKey, matchClause = 'OR') {
     return listOfData.filter(dataItem => {
         let searchInData = arrOfKeys.map(key => {
             return dataItem[key]
         })
         return searchInData.reduce((didMatch, currentData) => {
-            if (searchClause === 'AND') {
+            if (matchClause === 'AND') {
                 return didMatch && matchStrings(currentData, searchKey)
             }
             return didMatch || matchStrings(currentData, searchKey)
