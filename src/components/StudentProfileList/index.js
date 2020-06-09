0670bb1a63ce6d829e690f4f48ea7873a6e78b43
surajhell88 <surajhell88@gmail.com>
2020-06-09 15:22:47 +0530

    styling changes to list, completing Part 2 of the challenge

--- src/components/StudentProfileList/index.js
+++ src/components/StudentProfileList/index.js
@@ -22,13 +22,17 @@ function StudentProfileList() {
     <ul className="student-profile-list">
       {studentList.map(profile => {
         return <li key={profile.id}>
-          <div>
-            <img src={profile.pic} alt={`${profile.firstName} ${profile.lastName}`} />
-            <h4>{`${profile.firstName} ${profile.lastName}`}</h4>
-            <p>Email: {profile.email}</p>
-            <p>Company: {profile.company}</p>
-            <p>Skill: {profile.skill}</p>
-            <p>Average: {profile.average}%</p>
+          <div className="student-profile-list-container">
+            <div className="student-profile-list-image">
+              <img src={profile.pic} alt={`${profile.firstName} ${profile.lastName}`} />
+            </div>
+            <div className="student-profile-list-info">
+              <h4>{`${profile.firstName} ${profile.lastName}`}</h4>
+              <p>Email: {profile.email}</p>
+              <p>Company: {profile.company}</p>
+              <p>Skill: {profile.skill}</p>
+              <p>Average: {profile.average}%</p>
+            </div>
           </div>
         </li>
       })}
